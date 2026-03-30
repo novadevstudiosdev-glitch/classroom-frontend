b219895dc05021e4bcd0fa5cdf6dbc1c84224bad
carla Miranda <waleilla99@gmail.com>
2026-03-30 19:30:54 -0300

    modificación del home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
 import { HomeView } from "@/features/home/views";
-import { NavbarLayout } from "@/shared/layouts";
-import { BackgroundOrbs } from "@/features/home/layout/BackgroundOrbs";
 
 export default function HomePage() {
-  return (
-    <NavbarLayout>
-      <div>
-        <BackgroundOrbs />
-        <HomeView />
-      </div>
-    </NavbarLayout>
-      
-  );
+  return <HomeView />;
 }
